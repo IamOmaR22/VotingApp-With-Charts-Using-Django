fa6f670268718401c502a7e3d5a8a84b574a39b8
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-17 01:16:15 +0600

    Url Routing

--- pollster/urls.py
+++ pollster/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', include('users.urls')),
 ]
---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from users import views as user_views
+
+urlpatterns = [
+
+    path('register/', user_views.register, name='register'),
+]
