8793790d9f87631915350a505f1dcc7ba6efb2cb
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-17 01:39:44 +0600

    django_crispy_forms

--- pollster/settings.py
+++ pollster/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
