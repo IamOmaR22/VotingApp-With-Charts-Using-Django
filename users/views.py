f2ac47fa85f2fc2827b4aeca2045088315b9f059
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-17 01:25:08 +0600

    Registration done

--- users/views.py
+++ users/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm # Django Default
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
-    
-    form = UserCreationForm()
+
+    if request.method == 'POST': # This is a POST Request
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')  # Grab the username that is submitted for now
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+
+    else:    # This is not a POST Request. We will just create a form
+
+        form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form':form})
