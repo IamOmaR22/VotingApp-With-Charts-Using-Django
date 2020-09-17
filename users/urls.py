33ab1d328d2d84afd03599ae9b0c7551d80fbbcb
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-18 00:54:36 +0600

    Profile routing and viewa

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -17,3 +17,8 @@ def register(request):
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form':form})
+
+
+def profile(request):
+
+    return render(request, 'users/profile.html')
