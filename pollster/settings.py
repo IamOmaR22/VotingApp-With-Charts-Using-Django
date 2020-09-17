29a10da8254add7e8f273fc89688651e7a57f35b
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-18 00:06:24 +0600

    message alert

--- pollster/settings.py
+++ pollster/settings.py
@@ -123,3 +123,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')  # Grab the username that is submitted for now
             messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            return redirect('index')
 
     else:    # This is not a POST Request. We will just create a form
 
