3670dbb394228389e4df375377dbcce74711b5d4
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-19 01:25:31 +0600

    Update The UserProfile

--- users/views.py
+++ users/views.py
@@ -20,12 +20,24 @@ def register(request):
 
     return render(request, 'users/register.html', {'form':form})
 
+
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if request.method == 'POST':   
+        u_form = UserUpdateForm(request.POST, instance=request.user)  
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)  
 
-    context = {
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = { 
         'u_form': u_form,
         'p_form': p_form
     }
