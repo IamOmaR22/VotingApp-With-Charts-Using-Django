fe9779ea4065745436ee266084481a4a48672ffc
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-19 01:10:20 +0600

    Display Images On Profile Page

--- pollster/urls.py
+++ pollster/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -22,3 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
